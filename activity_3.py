c933f193bf1f0c97dd22a90496d89102ab495f9c
coder-50-glitch <dharrajashree74@gmail.com>
2025-04-10 13:02:25 +0530

    Add files via upload

---
+++ activity_3.py
@@ -0,0 +1,9 @@
+age=input("enter age")
+print("datatype of age is ", type(age))
+price = 34.90
+print("datatype of price is ", type(price))
+# after type casting
+age=str(age)
+print("datatype of age is ", type(age))
+price=int(price)
+print("datatype of price is ", type(price))
