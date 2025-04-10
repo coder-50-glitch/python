39b8963997f969bda79dba0023569d15564812d4
coder-50-glitch <dharrajashree74@gmail.com>
2025-04-10 12:55:42 +0530

    Add files via upload

---
+++ activity_2.py
@@ -0,0 +1,8 @@
+a=4
+print("data type of a is ", type(a))
+b=2.5
+print("data type of b is ", type(b))
+c="Kolkata"
+print("data type of ca is ", type(c))
+d=True
+print("data type of d is ", type(d))
