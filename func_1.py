a9f77741aafdcea766e1fe70c1f54616b617c793
coder-50-glitch <dharrajashree74@gmail.com>
2025-04-22 23:37:21 +0530

    Add files via upload

---
+++ func_1.py
@@ -0,0 +1,10 @@
+def welwish():
+    print("welcome")
+    print("to codingal")
+    
+welwish()
+def weather_cond():
+    print("the weather is pleasant in",autum)
+
+autum = "spring"    
+weather_cond()
