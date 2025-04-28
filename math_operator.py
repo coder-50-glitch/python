9a6a497e9474022eaf54c62467de0cd89fbc61a6
coder-50-glitch <dharrajashree74@gmail.com>
2025-04-29 00:17:58 +0530

    Add files via upload

---
+++ math_operator.py
@@ -0,0 +1,7 @@
+import math
+print("the floor n ceil value od 23.56 are", str(math.floor(23.56)), " ", str(math.ceil(23.56)))
+x=10
+y=-15
+print(str(math.copysign(x,y)))
+print(str(math.fabs(-36)))
+print(str(math.gcd(24,36)))
