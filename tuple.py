30463d2e2cc3b2adaec1b7614cc8fd435037e009
coder-50-glitch <dharrajashree74@gmail.com>
2025-05-07 02:13:42 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,13 @@
+tuplex =("tuple", False, 3.5, 1)
+print(tuplex)
+tuplex = (4,6,7,3,2,1)
+print(tuplex)
+tuplex=tuplex + (91,)
+print(tuplex)
+tuple1=(50,34,78,50,29)
+print(tuple1.count(50))
+tuplex = (2,4,3,5,1,6,7,8)
+_slice = tuplex[3:5]
+print(_slice)
+_slice=tuplex[:6]
+print(_slice)
