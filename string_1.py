6525fcb52dbea4858ea211dae4ea91d3d92a81d8
coder-50-glitch <dharrajashree74@gmail.com>
2025-11-06 20:15:39 +0530

    Add files via upload

---
+++ string_1.py
@@ -0,0 +1,15 @@
+word = "anaya and ani are playing"
+wd=""
+wdmax=""
+max=0
+print(word.split(' '))
+a=word.split(' ')
+print(a)
+for i in a:
+    wd=i
+    if(wd[0]=='a'):
+        print(wd)
+     #   print(wd)
+    #   max=len(wd)
+      #  wdmax=wd
+#print(wdmax)
